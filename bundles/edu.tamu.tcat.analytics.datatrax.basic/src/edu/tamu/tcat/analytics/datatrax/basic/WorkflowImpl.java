334e416df7254628e4cfa828104dc9a61d0349ea
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2014-09-28 15:08:34 -0500

    Stub implementation of a workflow
    https://issues.citd.tamu.edu/browse/DTL-7

--- bundles/edu.tamu.tcat.analytics.datatrax.basic/src/edu/tamu/tcat/analytics/datatrax/basic/WorkflowImpl.java
+++ bundles/edu.tamu.tcat.analytics.datatrax.basic/src/edu/tamu/tcat/analytics/datatrax/basic/WorkflowImpl.java
@@ -1,80 +1,52 @@
 package edu.tamu.tcat.analytics.datatrax.basic;
 
+import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 import edu.tamu.tcat.analytics.datatrax.DataTransformWorkflow;
-import edu.tamu.tcat.analytics.datatrax.InvalidTransformerConfiguration;
+import edu.tamu.tcat.analytics.datatrax.Transformer;
 
 public class WorkflowImpl<IN, OUT> implements DataTransformWorkflow<IN, OUT>
 {
 
+   // TODO monitor performance of different transformers
+   // TODO provide integrated 
    
-   public WorkflowImpl()
+   private final List<Transformer<?, ?>> transformers;
+   
+   public WorkflowImpl(List<Transformer<?, ?>> transformers)
    {
-      // TODO Auto-generated constructor stub
+      this.transformers = transformers;;
    }
-   
-//   private static List<Transformer<?, ?>> loadFactories(WorkflowConfiguration config) 
-//         throws InvalidTransformerConfiguration
-//   {
-//      List<Transformer<?, ?>> factories = new ArrayList<>();
-//      List<TransformerConfiguration> elements = config.getTransformers();
-//      for (TransformerConfiguration transCfg : elements)
-//      {
-//         Transformer<?, ?> factory = getTransformerFactory(transCfg.getTransformerId());
-//         checkTypeCompatibility(factories, factory);
-//         
-//         factory.configure(transCfg.getConfiguration());
-//         factories.add(factory);
-//      }
-//      
-//      return factories;
-//   }
-   
-//      FastSauvolaFactory thresholder = new TranformerFactory();
-//      thresholder.setK(0.3);
-////      thresholder.setWindowSize(15);
-//      Path imagePath = dataDir.resolve("shipbuilding-treatise.jpg");
-//      Path outputPath = dataDir.resolve("output/shipbuilding-treatise.png");
-//      
-//      final BufferedImage image = ImageIO.read(Files.newInputStream(imagePath, StandardOpenOption.READ));
-//      
-//      IntegralImageImpl iImage = IntegralImageImpl.create(image.getData());
-//      Runnable runnable = thresholder.create(() -> { return iImage; }, (im) -> {
-//         try
-      
 
-   /**
-    * Checks to ensure that the source type of the candidate 
-    * 
-    * @param pipeline
-    * @param tail
-    * @throws InvalidTransformerConfiguration
-    */
-//   private static void checkTypeCompatibility(List<Transformer<?, ?>> pipeline, Transformer<?, ?> tail) throws InvalidTransformerConfiguration
-//   {
-//      if (pipeline.size() == 0)
-//         return;
-//      
-//      Transformer<?, ?> previous = pipeline.get(pipeline.size() - 1);
-//      Class<?> outType = previous.getOutputType();
-//      Class<?> srcType = tail.getSourceType();
-//      if (!srcType.isAssignableFrom(outType))
-//      {
-//         // TODO add description
-//         throw new InvalidTransformerConfiguration();
-//      }
-//   }
-//   
-//   private static Transformer<?, ?> getTransformerFactory(String id)
-//   {
-//      // TODO implement me
-//      return null;
-//   }
-   
+   @Override
+   public Class<?> getSourceType()
+   {
+      Transformer<?, ?> transformer = transformers.get(0);
+      return transformer.getSourceType();
+   }
+
+   @Override
+   public Class<?> getOutputType()
+   {
+      Transformer<?, ?> transformer = transformers.get(transformers.size() - 1);
+      return transformer.getOutputType();
+   }
+
+   @Override
+   public boolean canAccept(Class<?> type)
+   {
+      return getSourceType().isAssignableFrom(type);
+   }
+
+   @Override
+   public boolean canProduce(Class<?> type) 
+   {
+      return type.isAssignableFrom(getOutputType());
+   }
+
    public void process(Supplier<IN> input, Consumer<OUT> ears)
    {
    }
-
 }
