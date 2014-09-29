d896880cb8993d9a41d6d0293ebc56b6ea005eaf
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2014-09-29 17:46:59 -0500

    Finished CC test case for DataTrax
    https://issues.citd.tamu.edu/browse/DTL-7

--- tests/edu.tamu.tcat.analytics.datatrax.tests/src/edu/tamu/tcat/analytics/datatrax/tests/WorkflowTests.java
+++ tests/edu.tamu.tcat.analytics.datatrax.tests/src/edu/tamu/tcat/analytics/datatrax/tests/WorkflowTests.java
@@ -2,9 +2,21 @@ package edu.tamu.tcat.analytics.datatrax.tests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+import javax.imageio.ImageIO;
 
 import org.junit.After;
 import org.junit.Before;
@@ -18,8 +30,11 @@ import edu.tamu.tcat.analytics.datatrax.basic.WorkflowConfigBuilderImpl;
 import edu.tamu.tcat.analytics.datatrax.basic.WorkflowFactoryImpl;
 import edu.tamu.tcat.analytics.datatrax.basic.factorymeta.ExtPointTranformerFactoryRegistry;
 import edu.tamu.tcat.analytics.image.integral.datatrax.BufferedImageAdapter;
+import edu.tamu.tcat.dia.binarization.datatrax.BinaryImageWriter;
 import edu.tamu.tcat.dia.binarization.sauvola.FastSauvolaTransformer;
+import edu.tamu.tcat.dia.segmentation.cc.ConnectComponentSet;
 import edu.tamu.tcat.dia.segmentation.cc.twopass.CCAnalyzer;
+import edu.tamu.tcat.dia.segmentation.cc.twopass.CCWriter;
 
 public class WorkflowTests
 {
@@ -95,19 +110,72 @@ public class WorkflowTests
       WorkflowFactoryImpl factory = new WorkflowFactoryImpl();
       factory.bindFactoryRegistry(registry);
       
-      DataTransformWorkflow<?, ?> workflow = factory.create(cfg);
+      @SuppressWarnings("rawtypes")
+      DataTransformWorkflow workflow = factory.create(cfg);
       assertNotNull("Failed to create workflow", workflow);
+      assertEquals("Mismatched workflow source type", BufferedImage.class, workflow.getSourceType());
+      assertEquals("Mismatched workflow output type", ConnectComponentSet.class, workflow.getOutputType());
    }
    
    @Test
-   public void testWorkflowExecution()
+   public void testWorkflowExecution() throws IOException, WorkflowConfigurationException, InterruptedException
    {
+      Path dataDir = Paths.get("C:\\dev\\git\\citd.dia\\tests\\edu.tamu.tcat.dia.binarization.sauvola.test\\res");
       
+      FastSauvolaTransformer thresholder = new FastSauvolaTransformer();
+      thresholder.setK(0.3);
+      Path imagePath = dataDir.resolve("shipbuilding-treatise.jpg");
+      
+      final BufferedImage image = ImageIO.read(Files.newInputStream(imagePath, StandardOpenOption.READ));
+      Path outputPath = dataDir.resolve("output/shipbuilding-treatise-bin.png");
+      Map<String, Object> binWriterParams = new HashMap<>();
+      binWriterParams.put("model", image);
+      binWriterParams.put("path", outputPath);
+      binWriterParams.put("format", "png");
+      
+      Map<String, Object> ccWriterParams = new HashMap<>();
+      outputPath = dataDir.resolve("output/shipbuilding-treatise-cc.png");
+      ccWriterParams.put("model", image);
+      ccWriterParams.put("path", outputPath);
+      ccWriterParams.put("format", "png");
+      
+      WorkflowConfigBuilderImpl builder = new WorkflowConfigBuilderImpl(registry);
+      builder.setTitle("Config Test");
+      builder.setDescription("A builder for use in testing builder creation");
+      
+      builder.append(createCfg(BufferedImageAdapter.EXTENSION_ID, null));
+      builder.append(createCfg(FastSauvolaTransformer.EXTENSION_ID, null));
+      builder.append(createCfg(BinaryImageWriter.EXTENSION_ID, binWriterParams));
+      builder.append(createCfg(CCAnalyzer.EXTENSION_ID, null));
+      builder.append(createCfg(CCWriter.EXTENSION_ID, ccWriterParams));
+      
+
+      WorkflowConfiguration cfg = builder.build();
+      
+      WorkflowFactoryImpl factory = new WorkflowFactoryImpl();
+      factory.bindFactoryRegistry(registry);
+      
+      DataTransformWorkflow<BufferedImage, ConnectComponentSet> workflow = factory.create(cfg);
+      final CountDownLatch latch = new CountDownLatch(1);
+      final AtomicReference<ConnectComponentSet> ccRef = new AtomicReference<>();
+      
+      workflow.process(() -> image, (ConnectComponentSet cc) -> {
+         ccRef.set(cc);
+         latch.countDown();
+      });
+      
+      latch.await(10, TimeUnit.SECONDS);
+      ConnectComponentSet cc = ccRef.get();
+      assertNotNull("No connected components returned", cc);
+      int size = cc.listLabels().size();
+      assertTrue("Surpisingly few connected components returned [" + size + "]", size > 500);
    }
    
-   @Test
+//   @Test
    public void testWorkflowConfigSerialization()
    {
       
    }
+   
+
 }
