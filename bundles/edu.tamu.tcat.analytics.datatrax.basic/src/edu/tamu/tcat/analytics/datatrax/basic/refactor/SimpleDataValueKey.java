e21843dcdaf335067c7eb817d8e173ed7b30ab92
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2014-11-14 15:20:29 -0600

    Implemented SimpleDataValueKey

--- bundles/edu.tamu.tcat.analytics.datatrax.basic/src/edu/tamu/tcat/analytics/datatrax/basic/refactor/SimpleDataValueKey.java
+++ bundles/edu.tamu.tcat.analytics.datatrax.basic/src/edu/tamu/tcat/analytics/datatrax/basic/refactor/SimpleDataValueKey.java
@@ -1,20 +1,54 @@
 package edu.tamu.tcat.analytics.datatrax.basic.refactor;
 
+import java.util.Objects;
+
 import edu.tamu.tcat.analytics.datatrax.DataValueKey;
 
 public class SimpleDataValueKey implements DataValueKey
 {
+   
+   private final Class<?> type;
+   private final String sourceId;
+
+   public SimpleDataValueKey(String sourceId, Class<?> type)
+   {
+      Objects.requireNonNull(sourceId);
+      Objects.requireNonNull(type);
+      
+      this.sourceId = sourceId;
+      this.type = type;
+   }
+   
    @Override
    public String getTransformerId()
    {
-      // TODO Auto-generated method stub
-      return null;
+      return sourceId;
    }
 
    @Override
    public <X> Class<X> getType()
    {
-      // TODO Auto-generated method stub
-      return null;
+      return (Class)type;
+   }
+   
+   @Override
+   public int hashCode()
+   {
+      int result = 47;
+      
+      result = 37 * result + type.hashCode();
+      result = 37 * result + sourceId.hashCode();
+      
+      return super.hashCode();
+   }
+   
+   @Override
+   public boolean equals(Object obj)
+   {
+      if (!DataValueKey.class.isInstance(obj))
+         return false;
+      
+      DataValueKey key = DataValueKey.class.cast(obj);
+      return type.equals(key.getType()) && sourceId.equalsIgnoreCase(key.getTransformerId());
    }
 }
