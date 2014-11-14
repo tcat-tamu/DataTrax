4fd8a0d9702190088386813a3319499a90fbdbf4
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2014-11-14 15:23:12 -0600

    Simplified TaskExecutionService interface

--- bundles/edu.tamu.tcat.analytics.datatrax.basic/src/edu/tamu/tcat/analytics/datatrax/basic/refactor/TaskExecutionService.java
+++ bundles/edu.tamu.tcat.analytics.datatrax.basic/src/edu/tamu/tcat/analytics/datatrax/basic/refactor/TaskExecutionService.java
@@ -1,10 +1,8 @@
 package edu.tamu.tcat.analytics.datatrax.basic.refactor;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.Future;
 
 public interface TaskExecutionService
 {
-   <X> Future<X> execute(Callable<X> transformationTask);
+   void execute(Runnable task);
 
 }
