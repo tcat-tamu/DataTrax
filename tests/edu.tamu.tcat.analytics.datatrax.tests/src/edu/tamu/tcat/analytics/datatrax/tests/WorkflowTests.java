afa75f42a2942e0843da4321b5805ffaac192dce
Neal Audenaert <nealaudenaert@tees.tamu.edu>
2015-01-14 13:37:23 -0600

    Implemented workflow execution unit test

--- tests/edu.tamu.tcat.analytics.datatrax.tests/src/edu/tamu/tcat/analytics/datatrax/tests/WorkflowTests.java
+++ tests/edu.tamu.tcat.analytics.datatrax.tests/src/edu/tamu/tcat/analytics/datatrax/tests/WorkflowTests.java
@@ -7,14 +7,18 @@ import static org.junit.Assert.assertTrue;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import javax.imageio.ImageIO;
 
@@ -26,6 +30,7 @@ import edu.tamu.tcat.analytics.datatrax.DataValueKey;
 import edu.tamu.tcat.analytics.datatrax.FactoryUnavailableException;
 import edu.tamu.tcat.analytics.datatrax.ResultsCollector;
 import edu.tamu.tcat.analytics.datatrax.TransformerConfigurationException;
+import edu.tamu.tcat.analytics.datatrax.basic.SimpleDataValueKey;
 import edu.tamu.tcat.analytics.datatrax.basic.WorkflowConfigBuilderImpl;
 import edu.tamu.tcat.analytics.datatrax.basic.WorkflowControllerImpl;
 import edu.tamu.tcat.analytics.datatrax.basic.factorymeta.ExtPointTranformerFactoryRegistry;
@@ -138,6 +143,43 @@ public class WorkflowTests
    }
    
    @Test
+   public void testOutputDefinition() throws Exception
+   {
+      WorkflowConfigurationBuilder builder = new WorkflowConfigBuilderImpl(registry);
+      builder.setTitle(DEFAULT_TITLE);
+      builder.setDescription(DEFAULT_DESC);
+      builder.setInputType(BufferedImage.class);
+      
+      // create thresholder configurations
+      DataValueKey inputKey = builder.getInputKey();
+      TransformerConfiguration integralImageAdapter = createIntegralImageAdapter(builder, inputKey);
+      TransformerConfiguration thresholder = createThresholder(builder, integralImageAdapter);
+      TransformerConfiguration ccAnalyzer = createCCAnalyzer(builder, thresholder);
+      TransformerConfiguration ccWriter = createCCWriter(builder, inputKey, ccAnalyzer);
+      
+      // declare (valid) outputs
+      builder.registerOutput(ccWriter.getId());
+      builder.registerOutput(ccAnalyzer.getId());
+      SimpleDataValueKey ccAnalyzerOutputPin = new SimpleDataValueKey(ccAnalyzer.getId(), ccAnalyzer.getOutputType());
+      SimpleDataValueKey ccWriterOutputPin = new SimpleDataValueKey(ccWriter.getId(), ccWriter.getOutputType());
+      
+      try
+      {
+         builder.registerOutput(UUID.randomUUID());
+         assertFalse("Attempt to register output for unknown transformer succeeded", true);
+      } catch (WorkflowConfigurationException wce)
+      {
+         // expected case.
+      }
+      
+      WorkflowConfiguration build = builder.build();
+      Set<DataValueKey> outputs = build.getDeclaredOutputs();
+      
+      assertTrue("Did not find data output for CC Analyzer", outputs.contains(ccAnalyzerOutputPin));
+      assertTrue("Did not find data output for CC Writer", outputs.contains(ccWriterOutputPin));
+   }
+   
+   @Test
    public void testWorkflowExecution() throws IOException, WorkflowConfigurationException, InterruptedException, FactoryUnavailableException, TransformerConfigurationException
    {
       WorkflowConfigurationBuilder builder = new WorkflowConfigBuilderImpl(registry);
@@ -146,15 +188,18 @@ public class WorkflowTests
       
       WorkflowControllerImpl workflow = createCCAnalyzerWorkflow(builder);
       
-      
       Path dataDir = Paths.get("C:\\dev\\git\\citd.dia\\tests\\edu.tamu.tcat.dia.binarization.sauvola.test\\res");
-      Path imagePath = dataDir.resolve("00000008.jp2");
+      Path imagePath = dataDir.resolve("00000009.jp2");
+      assertTrue("Source image does not exist. " + imagePath.toString(), Files.exists(imagePath));
+      
       BufferedImage image = ImageIO.read(imagePath.toFile());
       
       CountDownLatch latch = new CountDownLatch(2);
       List<Exception> errors = new CopyOnWriteArrayList<>();
       Map<DataValueKey, Object> outputs = new HashMap<>();
+      AtomicReference<BufferedImage> colorizedImage = new AtomicReference<>();
       
+      long start = System.currentTimeMillis();
       workflow.process(image, new ResultsCollector()
       {
          
@@ -162,124 +207,118 @@ public class WorkflowTests
          public <X> void handleResult(TranformationResult result)
          {
             outputs.put(result.getKey(), result.getValue());
+            if (result.getValue() instanceof BufferedImage)
+               colorizedImage.set((BufferedImage)result.getValue());
+            
             latch.countDown();
          }
          
          @Override
          public void handleError(TransformationError error)
          {
-            errors.add(error.getException());
+            Exception ex = error.getException();
+            errors.add(ex);
+            ex.printStackTrace();
          }
          
          @Override
          public void finished()
          {
-            // TODO alternatively, we could just wait untill processing is done
+            // TODO alternatively, we could just wait until processing is done
             System.out.println("Done.");
          }
       });
       
-      if (latch.await(2, TimeUnit.SECONDS))
+      if (latch.await(2, TimeUnit.HOURS))
       {
-         // TODO evaluate results
+         System.out.println("Time Elapsed: " + (System.currentTimeMillis() - start));
+         
+         Path output = dataDir.resolve("outputs").resolve("00000009.jpg");
+         Files.createDirectories(output);
+         ImageIO.write(colorizedImage.get(), "jpg", output.toFile());
       }
       else 
       {
          assertFalse("Failed to recieve results.", true);
       }
-      
-      // WIP execute configured workflow
-      
-//      
-      
-//      
-//      FastSauvolaTransformer thresholder = new FastSauvolaTransformer();
-//      thresholder.setK(0.3);
-//      Path imagePath = dataDir.resolve("shipbuilding-treatise.jpg");
-//      
-//      final BufferedImage image = ImageIO.read(Files.newInputStream(imagePath, StandardOpenOption.READ));
-//      Path outputPath = dataDir.resolve("output/shipbuilding-treatise-bin.png");
-//      Map<String, Object> binWriterParams = new HashMap<>();
-//      binWriterParams.put("model", image);
-//      binWriterParams.put("path", outputPath);
-//      binWriterParams.put("format", "png");
-//      
-//      Map<String, Object> ccWriterParams = new HashMap<>();
-//      outputPath = dataDir.resolve("output/shipbuilding-treatise-cc.png");
-//      ccWriterParams.put("model", image);
-//      ccWriterParams.put("path", outputPath);
-//      ccWriterParams.put("format", "png");
-//      
-//      WorkflowConfigBuilderImpl builder = new WorkflowConfigBuilderImpl(registry);
-//      builder.setTitle(DEFAULT_TITLE);
-//      builder.setDescription(DEFAULT_DESC);
-//      
-//      builder.append(createCfg(BufferedImageAdapter.EXTENSION_ID, null));
-//      builder.append(createCfg(FastSauvolaTransformer.EXTENSION_ID, null));
-//      builder.append(createCfg(BinaryImageWriter.EXTENSION_ID, binWriterParams));
-//      builder.append(createCfg(CCAnalyzer.EXTENSION_ID, null));
-//      builder.append(createCfg(CCWriter.EXTENSION_ID, ccWriterParams));
-////      
-//
-//      WorkflowConfiguration cfg = builder.build();
-//      
-//      WorkflowFactoryImpl factory = new WorkflowFactoryImpl();
-//      factory.bindFactoryRegistry(registry);
-//      
-//      DataTransformWorkflow<BufferedImage, ConnectComponentSet> workflow = factory.create(cfg);
-//      final CountDownLatch latch = new CountDownLatch(1);
-//      final AtomicReference<ConnectComponentSet> ccRef = new AtomicReference<>();
-//      
-//      workflow.process(() -> image, (ConnectComponentSet cc) -> {
-//         ccRef.set(cc);
-//         latch.countDown();
-//      });
-//      
-//      latch.await(10, TimeUnit.SECONDS);
-//      ConnectComponentSet cc = ccRef.get();
-//      assertNotNull("No connected components returned", cc);
-//      int size = cc.listLabels().size();
-//      assertTrue("Surpisingly few connected components returned [" + size + "]", size > 500);
    }
 
-   private WorkflowControllerImpl createCCAnalyzerWorkflow(WorkflowConfigurationBuilder builder) throws FactoryUnavailableException, WorkflowConfigurationException, TransformerConfigurationException
+   /**
+    * Constructs a simple workflow that accepts  a buffered image and generates a set 
+    * of connected components along with colorized image that displays those CCs.
+    * 
+    * @param builder
+    * @return
+    * 
+    * @throws FactoryUnavailableException
+    * @throws WorkflowConfigurationException
+    * @throws TransformerConfigurationException
+    */
+   private WorkflowControllerImpl createCCAnalyzerWorkflow(WorkflowConfigurationBuilder builder) 
+         throws FactoryUnavailableException, WorkflowConfigurationException, TransformerConfigurationException
    {
-      ExtTransformerFactoryDefinition buffImageReg = registry.getRegistration(BufferedImageAdapter.EXTENSION_ID);
-      ExtTransformerFactoryDefinition sauvolaReg = registry.getRegistration(FastSauvolaTransformer.EXTENSION_ID);
-      ExtTransformerFactoryDefinition ccReg = registry.getRegistration(CCAnalyzer.EXTENSION_ID);
-      ExtTransformerFactoryDefinition ccWriter = registry.getRegistration(CCWriter.EXTENSION_ID);
-
-      // construct a simple workflow that will accept a buffered image and generate a set 
-      // of connected components and a colorized image that displays those CCs. 
-      
-      
       builder.setInputType(BufferedImage.class);
       
-      // convert the input data to an integral image 
+      // create thresholder configurations
+      DataValueKey inputKey = builder.getInputKey();
+      TransformerConfiguration integralImageAdapter = createIntegralImageAdapter(builder, inputKey);
+      TransformerConfiguration thresholder = createThresholder(builder, integralImageAdapter);
+      TransformerConfiguration ccAnalyzer = createCCAnalyzer(builder, thresholder);
+      TransformerConfiguration ccWriter = createCCWriter(builder, inputKey, ccAnalyzer);
+      
+      // declare outputs
+      builder.registerOutput(ccWriter.getId());
+      builder.registerOutput(ccAnalyzer.getId());
+      
+      WorkflowConfiguration configuration = builder.build();
+      WorkflowControllerImpl workflow = WorkflowControllerImpl.create(configuration);
+      
+      return workflow;
+   }
+
+   private TransformerConfiguration createCCWriter(WorkflowConfigurationBuilder builder, DataValueKey imageModelProvider, TransformerConfiguration ccSetProvider) throws FactoryUnavailableException, WorkflowConfigurationException, TransformerConfigurationException
+   {
+      TransformerConfigEditor editor;
+      ExtTransformerFactoryDefinition ccWriterReg = registry.getRegistration(CCWriter.EXTENSION_ID);
+      
+      editor = builder.createTransformer(ccWriterReg);
+      editor.setDataSource(ccWriterReg.getDeclaredInput(CCWriter.CONNECTED_COMPONENTS_PIN), ccSetProvider);
+      editor.setDataSource(ccWriterReg.getDeclaredInput(CCWriter.MODEL_PIN), imageModelProvider);
+      TransformerConfiguration ccWriter = editor.getConfiguration();
+      return ccWriter;
+   }
+
+   private TransformerConfiguration createCCAnalyzer(WorkflowConfigurationBuilder builder, TransformerConfiguration binaryImageSource) throws FactoryUnavailableException, WorkflowConfigurationException, TransformerConfigurationException
+   {
+      TransformerConfigEditor editor;
+      ExtTransformerFactoryDefinition ccReg = registry.getRegistration(CCAnalyzer.EXTENSION_ID);
+      
+      editor = builder.createTransformer(ccReg);
+      editor.setDataSource(ccReg.getDeclaredInput(CCAnalyzer.BINARY_IMAGE_PIN), binaryImageSource);
+      TransformerConfiguration ccAnalyzer = editor.getConfiguration();
+      return ccAnalyzer;
+   }
+
+   private TransformerConfiguration createThresholder(WorkflowConfigurationBuilder builder, TransformerConfiguration integralImageDataSource) throws FactoryUnavailableException, WorkflowConfigurationException, TransformerConfigurationException
+   {
+      TransformerConfigEditor editor;
+      ExtTransformerFactoryDefinition sauvolaReg = registry.getRegistration(FastSauvolaTransformer.EXTENSION_ID);
+      
+      editor = builder.createTransformer(sauvolaReg);
+      editor.setDataSource(sauvolaReg.getDeclaredInput(FastSauvolaTransformer.INTEGRAL_IMAGE_PIN), integralImageDataSource);
+      TransformerConfiguration thresholder = editor.getConfiguration();
+      return thresholder;
+   }
+
+   private TransformerConfiguration createIntegralImageAdapter(WorkflowConfigurationBuilder builder, DataValueKey sourceData) throws WorkflowConfigurationException, TransformerConfigurationException, FactoryUnavailableException
+   {
+      ExtTransformerFactoryDefinition buffImageReg = registry.getRegistration(BufferedImageAdapter.EXTENSION_ID);
+
       TransformerConfigEditor editor = builder.createTransformer(buffImageReg);
       editor.setDataSource(buffImageReg.getDeclaredInput(BufferedImageAdapter.IMAGE_PIN), builder.getInputKey());
       TransformerConfiguration integralImageAdapter = editor.getConfiguration();
       
-      // threshold the integral image
-      editor = builder.createTransformer(sauvolaReg);
-      editor.setDataSource(sauvolaReg.getDeclaredInput(FastSauvolaTransformer.INTEGRAL_IMAGE_PIN), integralImageAdapter);
-      TransformerConfiguration thresholder = editor.getConfiguration();
-      
-      // perform CC analysis
-      editor = builder.createTransformer(ccReg);
-      editor.setDataSource(ccReg.getDeclaredInput(CCAnalyzer.BINARY_IMAGE_PIN), thresholder);
-      TransformerConfiguration ccAnalyzer = editor.getConfiguration();
-      
-      editor = builder.createTransformer(ccWriter);
-      editor.setDataSource(ccWriter.getDeclaredInput(CCWriter.CONNECTED_COMPONENTS_PIN), ccAnalyzer);
-      editor.setDataSource(ccWriter.getDeclaredInput(CCWriter.MODEL_PIN), builder.getInputKey());
-      
-      // TODO declare outputs
-      
-      
-      WorkflowConfiguration configuration = builder.build();
-      WorkflowControllerImpl workflow = WorkflowControllerImpl.create(configuration);
-      return workflow;
+      return integralImageAdapter;
    }
    
 //   @Test
